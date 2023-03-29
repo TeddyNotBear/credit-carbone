a0e9e2282beda98a8c117711dd3556e0e35a6df1
Teddy Not Bear <notbearteddy@gmail.com>
2023-03-29 12:58:14 +0200

    feat: add limit to json file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,13 +11,13 @@ import { config } from "dotenv";
 config({ path: require('find-config')('.env') });
 
 try {
-    mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 } catch (error) {
     console.log(error);   
 }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb', extended: true }));
 app.use(cors());
 
 buildRoutes(app);

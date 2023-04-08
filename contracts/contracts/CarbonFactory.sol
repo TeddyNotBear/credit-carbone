efef75686f3f0a814b8d7bf1cc7dead6f37c8eae
Nacer Fl <90337466+NacerFl@users.noreply.github.com>
2023-04-08 05:32:59 +0200

    factory finish

--- contracts/contracts/CarbonFactory.sol
+++ contracts/contracts/CarbonFactory.sol
@@ -1,20 +1,21 @@
 import "./SCC.sol";
 import "./UCO.sol";
 
-import "@openzeppelin/contracts-upgradeable/token/ERC1155/utils/ERC1155HolderUpgradeable.sol";
+//import "@openzeppelin/contracts-upgradeable/token/ERC1155/utils/ERC1155HolderUpgradeable.sol";
 // import './interface/IMyERC1155.sol';
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+//import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract CarbonFactory is
     Initializable,
-    ERC1155HolderUpgradeable,
     OwnableUpgradeable
 {
-    constructor() initializer {}
+    constructor(address _admin) initializer {
+        admin = _admin;
+    }
 
     mapping(address => address) public SCCstorage;
     mapping(address => address) public UCOstorage;
@@ -22,16 +23,22 @@ contract CarbonFactory is
     event UCODeployed(address _ucoAddress);
     address admin;
 
+
+    /*
     function initialize(address _admin) public initializer {
         admin = _admin;
     }
+    */
 
 
-    function deployUCO(address _adminAddr,string memory _name ,string memory _symbol) public {
+    function deployUCO(address _adminAddr,string memory _name ,string memory _symbol) public returns (address){
+        // créer un nouveau contrat UCO
         UCO uco = new UCO();
-        uco.initialize(_name,_symbol);
+        uco.initialize(_name, _symbol);
         UCOstorage[_adminAddr] = address(uco);
         emit UCODeployed(address(uco));
+        return address(uco);
+    
     }
 
 
@@ -39,11 +46,20 @@ contract CarbonFactory is
         address _to,
         string memory baseUri,
         address ucoAddr
-    ) public {
+    ) public returns (address) {
         SCC scc = new SCC();
         scc.initialize(baseUri,ucoAddr);
        // scc.mint(_to, uri, data,quantity);
         SCCstorage[_to] = address(scc);
         emit SCCDeployed(address(scc));
+        return address(scc);
+    }
+
+    function getSCCstorage(address _admin) public view returns(address){
+        return SCCstorage[_admin];
+    }
+
+        function getUCOstorage(address _admin) public view returns(address){
+        return UCOstorage[_admin];
     }
 }
--- contracts/test/carbon-fatcory-test.js
+++ contracts/test/carbon-fatcory-test.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 describe("CarbonFactory contract", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
    // const [owner] = await ethers.getSigners();
@@ -8,16 +8,31 @@ describe("CarbonFactory contract", function () {
    [deployer,account0, account1, account2, account3, account4,account5,account6, addr1, addr2, ...addrs] = await ethers.getSigners();
 
  
-    
+   let capturedValue
+   const captureValue = (value) => {
+       capturedValue = value
+       return true
+   }
 
       const CarbonFactory = await ethers.getContractFactory("CarbonFactory");
     
-      const CRB = await CarbonFactory.deploy();
+      const CRB = await CarbonFactory.deploy(deployer.address);
 
-    const deployContract = await CRB.deployUCO(deployer.address,'name','sym');
+      //const iniCRB = await CRB.initialize(deployer.address);
+      //iniCRB.wait();
+   
+    const deployContract = await CRB.deployUCO(deployer.address,'name','sym')
 
-    deployContract.wait();
-    console.log("events",await deployContract.events);
+    const contractReceipt =  deployContract.wait();
+
+    console.log("events",await deployContract);
+    console.log("value",await deployContract.value);
+
+    await expect(CRB.deployUCO(account1.address,'name','sym'))
+            .to.emit(CRB, 'UCODeployed')
+            .withArgs(captureValue)
+          console.log("CapturedValue:", capturedValue)
+
 
    // console.log("passed",deployContract);
 
